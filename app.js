af8625e9a9d7e5201233c7ebf22043940817892b
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-05 21:05:10 +0100

    Routing Requests

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http');
 const rqListener = (req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<html>');
+    res.write('<body><form action="/message" methed="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
