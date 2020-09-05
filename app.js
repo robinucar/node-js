a4159a84391109438e88c980398609f9a0534098
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-05 22:10:15 +0100

    Redirecting Request

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const http = require('http');
-const rqListener = (req, res) => {
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
-    res.write('<html>');
-    res.write('<body><form action="/message" methed="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
     return res.end();
   }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body>');
-  res.write('<h1>Node Js</h1>');
-  res.write('<p>I started to learn Node Js today.</p>');
-  res.write('</body>');
-  res.write('</html>');
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body>");
+  res.write("<h1>Node Js</h1>");
+  res.write("<p>I started to learn Node Js today.</p>");
+  res.write("</body>");
+  res.write("</html>");
   res.end();
-};
-
-const server = http.createServer(rqListener);
+});
 
 server.listen(3000);
